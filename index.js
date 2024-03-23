f1f8796e358bf1a3e0ab84009fb73f2f8359222f
jubayer2023 <jubayersarker400@gmail.com>
2024-03-23 12:14:10 +0600

    jwt-cookie-parser-installed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Requirements
 const express = require('express');
 const cors = require('cors');
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -22,117 +23,122 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	}
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-
-    // collection
-    const serviceCollection = client.db('carDoctor').collection('services');
-
-    const bookingCollection = client.db('carDoctor').collection('bookings');
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
 
 
-    app.get('/services', async (req, res) => {
-      const cursor = serviceCollection.find();
-      const services = await cursor.toArray();
-      res.send(services);
-    })
-
-    // dynamic id route
-    app.get('/services/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-
-      const options = {
-        projection: { _id: 1, title: 1, service_id: 1, price: 1, img: 1 }
-      }
-
-      const result = await serviceCollection.findOne(query, options);
-      res.send(result);
-    })
+		// auth related api
+		
 
 
-    // get bookingsinfo
-    app.get('/bookings', async (req, res) => {
+		// collection
+		const serviceCollection = client.db('carDoctor').collection('services');
 
-      // console.log(req.query.email);
-
-      let query = {};
-
-      if (req.query?.email) {
-        query = { email: req.query.email };
-      }
-      // console.log(query);
-
-      const result = await bookingCollection.find(query).toArray();
-      res.send(result);
-    })
+		const bookingCollection = client.db('carDoctor').collection('bookings');
 
 
+		app.get('/services', async (req, res) => {
+			const cursor = serviceCollection.find();
+			const services = await cursor.toArray();
+			res.send(services);
+		})
 
-    // booking route post method
-    app.post('/bookings', async (req, res) => {
-      const bookingInfo = req.body;
-      console.log(bookingInfo);
+		// dynamic id route
+		app.get('/services/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
 
-      const insertBookingInfo = await bookingCollection.insertOne(bookingInfo);
+			const options = {
+				projection: { _id: 1, title: 1, service_id: 1, price: 1, img: 1 }
+			}
 
-      res.send(insertBookingInfo);
-
-    })
-
-
-    // specific booking data delete
-    app.delete('/bookings/:id', async (req, res) => {
-
-      const id = req.params.id;
-
-      const query = { _id: new ObjectId(id) };
-
-      const result = await bookingCollection.deleteOne(query);
-
-      res.send(result);
-
-    })
+			const result = await serviceCollection.findOne(query, options);
+			res.send(result);
+		})
 
 
-    // update (PATCH) method
+		// get bookingsinfo
+		app.get('/bookings', async (req, res) => {
 
-    app.patch('/bookings/:id', async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
+			// console.log(req.query.email);
 
-      const updatedInfo = req.body;
-      // console.log(updateInfo);
+			let query = {};
 
-      const updateDoc = {
-        $set: {
-          status: updatedInfo.status,
-        },
-      };
+			if (req.query?.email) {
+				query = { email: req.query.email };
+			}
+			// console.log(query);
 
-      const result = await bookingCollection.updateOne(filter, updateDoc);
-      res.send(result);
-
-    })
+			const result = await bookingCollection.find(query).toArray();
+			res.send(result);
+		})
 
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+		// booking route post method
+		app.post('/bookings', async (req, res) => {
+			const bookingInfo = req.body;
+			console.log(bookingInfo);
+
+			const insertBookingInfo = await bookingCollection.insertOne(bookingInfo);
+
+			res.send(insertBookingInfo);
+
+		})
+
+
+		// specific booking data delete
+		app.delete('/bookings/:id', async (req, res) => {
+
+			const id = req.params.id;
+
+			const query = { _id: new ObjectId(id) };
+
+			const result = await bookingCollection.deleteOne(query);
+
+			res.send(result);
+
+		})
+
+
+		// update (PATCH) method
+
+		app.patch('/bookings/:id', async (req, res) => {
+			const id = req.params.id;
+			const filter = { _id: new ObjectId(id) };
+
+			const updatedInfo = req.body;
+			// console.log(updateInfo);
+
+			const updateDoc = {
+				$set: {
+					status: updatedInfo.status,
+				},
+			};
+
+			const result = await bookingCollection.updateOne(filter, updateDoc);
+			res.send(result);
+
+		})
+
+
+
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+	} finally {
+		// Ensures that the client will close when you finish/error
+		// await client.close();
+	}
 }
 run().catch(console.dir);
 
@@ -141,9 +147,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Car doctor server is running !!');
+	res.send('Car doctor server is running !!');
 })
 
 app.listen(port, () => {
-  console.log(`Car doctor server is running on port ${port}`);
+	console.log(`Car doctor server is running on port ${port}`);
 })
